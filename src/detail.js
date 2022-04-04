295a2086f55cf5bc1cd4eba27abe161693753943
qkrvudtn98 <ps3624@naver.com>
2022-04-04 18:25:00 +0900

    Make styling File

--- src/detail.js
+++ src/detail.js
@@ -1,5 +1,16 @@
 import React, { useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from 'styled-components';
+import './detail.scss';
+
+let Box = styled.div`
+    padding : 20px;
+`;
+
+let Title = styled.h4`
+    font-size : 25px;
+    color : ${ props => props.color }
+`;
 
 function Detail(props) {
     let { id } = useParams();
@@ -9,19 +20,27 @@ function Detail(props) {
 
     let history = useHistory();
     return (
-      <div className="container">
+      <div className="container">      
+        <Box>
+          <Title className="red">Detail</Title>
+        </Box>
+        
+        <div className="my-alert">
+          <p>재고가 얼마 남지 않았습니다</p>
+        </div>
+
         <div className="row">
           <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <img src={"https://codingapple1.github.io/shop/shoes" + (findItem.id + 1) + ".jpg"} width="100%" />
           </div>
           <div className="col-md-6 mt-4">
             <h4 className="pt-5">{findItem.title}</h4>
             <p>{findItem.content}</p>
             <p>{findItem.price}</p>
-              <button className="btn btn-danger"onClick={() => {
-                  history.goBack();
-              }}>뒤로가기</button> 
-              <button className="btn btn-danger">주문하기</button> 
+            <button className="btn btn-danger"onClick={() => {
+              history.goBack();
+            }}>뒤로가기</button> 
+            <button className="btn btn-danger">주문하기</button> 
           </div>
         </div>
       </div> 
